705dd3394618dfd2e96f4b3166f1edf6756388dd
mattcroat <mattcroat.dev@gmail.com>
2022-08-18 16:14:53 +0200

    chore: Update Context API example to use reactive declaration

--- src/lib/04-context/index.svelte
+++ src/lib/04-context/index.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-	import { setContext } from 'svelte'
-	import { readable, writable } from 'svelte/store'
+  import { setContext } from 'svelte'
+  import { readable, writable } from 'svelte/store'
 
-	import AddTodo from './add-todo.svelte'
-	import TodoList from './todo-list.svelte'
-	import TodosLeft from './todos-left.svelte'
+  import AddTodo from './add-todo.svelte'
+  import TodoList from './todo-list.svelte'
+  import TodosLeft from './todos-left.svelte'
 
-	import { createUniqueId } from '$lib/utils'
-	import type { Todo } from '$lib/types'
+  import { createUniqueId } from '$lib/utils'
+  import type { Todo } from '$lib/types'
 
-	// $todos.filter((todo) => !todo.completed).length
+  const todos = writable<Todo[]>([])
+  const todosLeft = writable(0)
 
-	const todos = writable<Todo[]>([])
-	const todosLeft = writable(0)
+  $: $todosLeft = $todos.filter((todo) => !todo.completed).length
 
-	setContext('todos', { todos, addTodo, removeTodo, todosLeft })
+  setContext('todos', { todos, addTodo, removeTodo, todosLeft })
 
-	function addTodo(todo: string) {
-		$todos = [
-			...$todos,
-			{
-				id: createUniqueId(),
-				description: todo,
-				completed: false,
-			},
-		]
-	}
+  function addTodo(todo: string) {
+    $todos = [
+      ...$todos,
+      {
+        id: createUniqueId(),
+        description: todo,
+        completed: false,
+      },
+    ]
+  }
 
-	function removeTodo(id: string) {
-		$todos = $todos.filter((todo) => todo.id !== id)
-	}
+  function removeTodo(id: string) {
+    $todos = $todos.filter((todo) => todo.id !== id)
+  }
 </script>
 
 <AddTodo />
